89f2333eb227324acebd40efd336a5a6e7b9b52e
sh <16303791+speedyhoon@users.noreply.github.com>
2018-05-02 15:03:12 +1000

    Add DateTime validation function.

--- v8.go
+++ v8.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/speedyhoon/forms"
 	"github.com/speedyhoon/utl"
+	"time"
 )
 
 const maxLen int = 64
@@ -93,4 +94,12 @@ func FileReq(f *forms.Field, inp ...string) {
 	//maxlength < 2MB?
 	//Unmarshal??
 	//Return as interface{}??
+}
+
+func DateTime(f *forms.Field, inp ...string) {
+	var err error
+	f.Value, err = time.Parse(f.Placeholder, inp[0])
+	if err != nil{
+		f.Err = err.Error()
+	}
 }
