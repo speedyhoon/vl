c9152977d846fb0637f1fd4cd8f802990477f352
sh <16303791+speedyhoon@users.noreply.github.com>
2020-01-09 09:02:32 +1100

    Add min & Max validation to DateTime

--- time.go
+++ time.go
@@ -1,6 +1,7 @@
 package vl
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/speedyhoon/frm"
@@ -8,9 +9,15 @@ import (
 
 //DateTime validates inp as a time.Time input
 func DateTime(f *frm.Field, inp ...string) {
-	var err error
-	f.Value, err = time.Parse(f.Placeholder, inp[0])
+	value, err := time.Parse(f.Placeholder, inp[0])
+	f.Value = value
 	if err != nil {
 		f.Err = err.Error()
+		return
+	}
+
+	if f.Min != nil && f.Min.(time.Time).After(value) || f.Max != nil && value.After(f.Max.(time.Time)) {
+		f.Err = fmt.Sprintf("Must be between %v and %v.", f.Min, f.Max)
+		return
 	}
 }
