6fa9010d377153ebf5ab65c782f97d83ff5db272
sh <16303791+speedyhoon@users.noreply.github.com>
2020-01-09 09:09:37 +1100

    Add validation for float64.

--- float.go
+++ float.go
@@ -29,14 +29,45 @@ func Float32(f *frm.Field, inp ...string) {
 		return
 	}
 
-	if rem := toFixed(math.Mod(f64, float64(f.Step)), 6); rem != 0 {
+	if rem := toFixed32(math.Mod(f64, float64(f.Step)), 6); rem != 0 {
 		f.Err = fmt.Sprintf("Please enter a valid value. The two nearest values are %v and %v.", num-rem, num-rem+f.Step)
 		return
 	}
 	f.Value = num
 }
 
-func toFixed(num, precision float64) float32 {
+//Float64 validates inp as a float64 input
+func Float64(f *frm.Field, inp ...string) {
+	num, err := strconv.ParseFloat(strings.TrimSpace(inp[0]), 64)
+	if err != nil {
+		//Return error if input string failed to convert.
+		f.Err = err.Error()
+		return
+	}
+
+	if !f.Required && num == 0 {
+		//f.ValueFloat64 is zero by default so assigning zero isn't required
+		return
+	}
+
+	if f.Min != nil && num < f.Min.(float64) || f.Max != nil && num > f.Max.(float64) {
+		f.Err = fmt.Sprintf("Must be between %v and %v.", f.Min, f.Max)
+		return
+	}
+
+	if rem := toFixed(math.Mod(num, float64(f.Step)), 6); rem != 0 {
+		f.Err = fmt.Sprintf("Please enter a valid value. The two nearest values are %v and %v.", num-rem, num-rem+float64(f.Step))
+		return
+	}
+	f.Value = num
+}
+
+func toFixed32(num, precision float64) float32 {
 	output := math.Pow(10, precision)
 	return float32(int(num*output)) / float32(output)
+}
+
+func toFixed(num, precision float64) float64 {
+	output := math.Pow(10, precision)
+	return float64(int(num*output)) / output
 }
